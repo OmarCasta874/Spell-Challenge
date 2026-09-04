d921717dc605a18640f360388611dcf607fe8e87
OmarCasta874 <castanonjc754@gmail.com>
2026-09-04 11:16:53 -0700

    imagenes del proyecto, vistas, urls y htmls

--- spellchallengefront/home/urls.py
+++ spellchallengefront/home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('login/', views.login, name='login'),
+    path('choose-role/', views.choose_role, name='choose_role'),
+    path('signup/student/', views.signup_student, name='signup_student'),
+    path('signup/teacher/', views.signup_teacher, name='signup_teacher'),
 ]
--- spellchallengefront/home/views.py
+++ spellchallengefront/home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request, 'base/landing.html')
+
+def login(request):
+    return render(request, 'users/login.html')
+
+def choose_role(request):
+    return render(request, 'base/choose_role.html')
+
+def signup_student(request):
+    return render(request, 'users/singup_student.html')
+
+def signup_teacher(request):
+    return render(request, 'users/singup_teacher.html')
