dc17c55c05da9f5b551d5955ee500a9bc4ae7f43
OmarCasta874 <castanonjc754@gmail.com>
2026-09-04 13:47:32 -0700

    ajuste de imágenes y creación de contenido en vista de student

--- spellchallengefront/home/urls.py
+++ spellchallengefront/home/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('choose-role/', views.choose_role, name='choose_role'),
     path('signup/student/', views.signup_student, name='signup_student'),
     path('signup/teacher/', views.signup_teacher, name='signup_teacher'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='teacher_dashboard'),
+    path('mygroups/', views.my_groups, name='teacher_groups'),
+    path('word_lists/', views.word_lists, name='teacher_word_lists'),
+    path('home/', views.student_home, name='student_home'),
+    path('mygroups_student/', views.student_groups, name='student_groups'),
+    path('practices/', views.practices, name='practices'),
 ]
